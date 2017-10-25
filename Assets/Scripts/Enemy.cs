4d00a98476555126cb962c6b4523347e28c61677
Colin Miller <colinjmiller93@gmail.com>
2017-10-24 23:06:54 -0700

    Moving fight control to FightManager; basic implementation of fight flow

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,72 +10,58 @@ public class Enemy : MonoBehaviour {
 	public float stageOneDuration;
 	public float stageTwoDuration;
 
+    public string BLOCKING = "blocking";
+    public string COMPLETE = "complete";
+
 	private LineRenderer lineRenderer;
 	private int currentWaypointIndex;
 	private int currentStage;
 	private float currentTime;
 
-	private bool inCombat;
-
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer> ();
-		inCombat = false;
-		reset ();
 	}
 
 	void Update () {
-		if (!inCombat) {
-			return;
-		}
-
 		currentTime += Time.deltaTime;
-		updateStage ();
-
-		if (currentStage == 1) {
-			showProjectedPath ();
-		} else if (currentStage == 2) {
-			hideProjectedPath ();
-		} else if (currentStage == 3) {
-			enemyMovements ();
-		}
 	}
 
 	void OnCollisionEnter2D() {
         Destroy (gameObject);
 	}
 
-	public void beginCombat() {
-		inCombat = true;
-		reset ();
-	}
+    public void setStage(int stage) {
+        currentStage = stage;
+        currentTime = 0f;
+    }
 
-	private void reset() {
-		currentTime = 0f;
-		currentStage = 0;
-		currentWaypointIndex = 0;
-	}
+    public int getStage() {
+        return currentStage;
+    }
 
-	private void updateStage() {
-		bool moveToNextStage = (currentStage == 0 && currentTime > stageZeroDuration);
-		moveToNextStage = moveToNextStage || (currentStage == 1 && currentTime > (stageZeroDuration + stageOneDuration));
-		moveToNextStage = moveToNextStage || (currentStage == 2 && currentTime > (stageZeroDuration + stageOneDuration + stageTwoDuration));
+    public string getStatus() {
+        if (currentStage == 0) {
+            return (currentTime > stageZeroDuration) ? COMPLETE : BLOCKING;
+        } else if (currentStage == 1) {
+            return (currentTime > stageOneDuration) ? COMPLETE : BLOCKING;
+        } else if (currentStage == 2) {
+            return (currentTime > stageTwoDuration) ? COMPLETE : BLOCKING;
+        } else {
+            return (currentWaypointIndex == waypoints.Length) ? COMPLETE : BLOCKING;
+        }
+    }
 
-		if (moveToNextStage) {
-			currentStage++;
-		}
-	}
-
-	private void showProjectedPath() {
+	public void showProjectedPath() {
 		lineRenderer.positionCount = waypoints.Length;
 		lineRenderer.SetPositions (waypoints);
 	}
 
-	private void hideProjectedPath() {
+	public void hideProjectedPath() {
 		lineRenderer.positionCount = 0;
 		lineRenderer.SetPositions(new Vector3[0]);
 	}
 
-    private void enemyMovements() {
+    public void move() {
         if (currentWaypointIndex < waypoints.Length) {
             Vector3 currentWaypoint = waypoints [currentWaypointIndex];
             transform.position = Vector3.MoveTowards (transform.position, currentWaypoint, speed * Time.deltaTime);
--- Assets/Scripts/FightManager.cs
+++ Assets/Scripts/FightManager.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class FightManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public GameObject cursor;
+    public GameObject fightText;
+    public GameObject itemText;
+    public GameObject escapeText;
+
+    public GameObject enemy;
+
+    private int TOTAL_OPTIONS = 3;
+    private int cursorPosition;
+    private bool enemyTurn;
+    private Enemy enemyAPI;
+
+    void Awake() {
+        cursorPosition = 2;
+        enemyTurn = false;
+        enemyAPI = enemy.GetComponent<Enemy> ();
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (enemyTurn && enemyAPI.getStatus() == enemyAPI.COMPLETE) {
+            Debug.Log ("Stage " + enemyAPI.getStage () + " has ended");
+            enemyAPI.setStage (enemyAPI.getStage() + 1);
+            enemyAPI.hideProjectedPath ();
+        }
+
+        if (enemyTurn && enemyAPI.getStage () == 1) {
+            enemyAPI.showProjectedPath ();
+        }
+
+        if (enemyTurn && enemyAPI.getStage () == 3) {
+            enemyAPI.move ();
+        }
+
+        checkForMenuChanges ();
+        if (Input.GetKeyDown (KeyCode.Space)) {
+            if (cursorPosition == 0) {
+                Debug.Log ("Attempt escape");
+            } else if (cursorPosition == 1) {
+                Debug.Log ("Call unimplemented item API");
+            } else {
+                enemyTurn = true;
+                enemyAPI.setStage (0);
+                Debug.Log ("Enemy turn begins");
+            }
+        }
 	}
+
+    private void checkForMenuChanges() {
+        if (Input.GetKeyDown (KeyCode.DownArrow)) {
+            cursorPosition--;
+            if (cursorPosition < 0) {
+                cursorPosition = TOTAL_OPTIONS - 1;
+            }
+        }
+        if (Input.GetKeyDown (KeyCode.UpArrow)) {
+            cursorPosition = (cursorPosition + 1) % TOTAL_OPTIONS;
+        }
+        cursor.transform.position = getCursorPosition ();
+    }
+
+    private Vector3 getCursorPosition() {
+        if (cursorPosition == 0) {
+            return new Vector3 (cursor.transform.position.x, escapeText.transform.position.y, 0);
+        } else if (cursorPosition == 1) {
+            return new Vector3 (cursor.transform.position.x, itemText.transform.position.y, 0);
+        } else {
+            return new Vector3 (cursor.transform.position.x, fightText.transform.position.y, 0);
+        }
+    }
 }
